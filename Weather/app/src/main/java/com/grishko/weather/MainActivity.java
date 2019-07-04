b7e8299351e407f296de8205f51904ce5140a15e
Haonara <masha-2007.555@mail.ru>
2019-07-05 09:10:23 +1000

    Вынесены в ресурсы ширина и высота текстовых блоков. Контекст приложения в сообщениях заменен на контекст активити.

--- Weather/app/src/main/java/com/grishko/weather/MainActivity.java
+++ Weather/app/src/main/java/com/grishko/weather/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "onStart()", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "onStart");
     }
 
@@ -20,54 +20,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "onCreate()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "onCreate()", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "onCreate");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle saveInstanceState){
         super.onRestoreInstanceState(saveInstanceState);
-        Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "onRestoreInstanceState");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "onResume()", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "onPause()", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "onPause");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle saveInstanceState){
         super.onSaveInstanceState(saveInstanceState);
-        Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "onSaveInstanceState");
     }
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "onStop()", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "onStop");
     }
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "onRestart()", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "onDestroy()", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "onDestroy");
     }
 
